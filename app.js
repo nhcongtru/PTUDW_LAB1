301877ffb2156b872279ac532957edd42ab11b26
Ngo Huynh Cong Tru <trub2014627@student.ctu.edu.vn>
2024-02-29 00:04:30 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-
 const contactsRouter = require("./app/routes/contact.route");
-
 const ApiError = require("./app/api-error");
-
 const app = express();
 
 
 app.use(cors());
 app.use(express.json());
-
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to contact book application."});
 });
-
-module.exports = app;
-
+app.use((req, res, next) => {
+    // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+    // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
+});
 app.use("/api/contacts", contactsRouter);
-
-
 // handle 404 response
 app.use((req, res, next) => {
 	// Code ở đây sẽ chạy khi không có route được định nghĩa nào
@@ -35,3 +31,5 @@ app.use((err, req, res, next) => {
 		message: error.message || "Internal Server Error",
 	});
 });
+
+module.exports = app;
